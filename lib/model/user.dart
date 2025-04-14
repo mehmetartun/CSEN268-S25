a97b8e3aac714ed193eefbaa191030adcd724b81
Mehmet Artun <mehmet@artun.com>
2025-04-13 22:09:04 -0700

    Lecture 5 Step 1

---
+++ lib/model/user.dart
@@ -0,0 +1,83 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class User {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String imageUrl;
+  final String uid;
+
+  User(
+      {required this.firstName,
+      required this.lastName,
+      required this.email,
+      required this.imageUrl,
+      required this.uid});
+
+  User copyWith({
+    String? firstName,
+    String? lastName,
+    String? email,
+    String? imageUrl,
+    String? uid,
+  }) {
+    return User(
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      email: email ?? this.email,
+      imageUrl: imageUrl ?? this.imageUrl,
+      uid: uid ?? this.uid,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'imageUrl': imageUrl,
+      'uid': uid,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      firstName: map['firstName'] as String,
+      lastName: map['lastName'] as String,
+      email: map['email'] as String,
+      imageUrl: map['imageUrl'] as String,
+      uid: map['uid'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) =>
+      User.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'User(firstName: $firstName, lastName: $lastName, email: $email, imageUrl: $imageUrl, uid: $uid)';
+  }
+
+  @override
+  bool operator ==(covariant User other) {
+    if (identical(this, other)) return true;
+
+    return other.firstName == firstName &&
+        other.lastName == lastName &&
+        other.email == email &&
+        other.imageUrl == imageUrl &&
+        other.uid == uid;
+  }
+
+  @override
+  int get hashCode {
+    return firstName.hashCode ^
+        lastName.hashCode ^
+        email.hashCode ^
+        imageUrl.hashCode ^
+        uid.hashCode;
+  }
+}
--- toc.md
+++ toc.md
@@ -3,3 +3,4 @@ The content in this repo follows the CSEN268 Spring25 Lecture at Santa Clara Uni
 
 - [Lecture 4 - 01](https://github.com/mehmetartun/CSEN268-S25/tree/L04.01) Intro to Flutter - Stateless Widget
 - [Lecture 5 - 00](https://github.com/mehmetartun/CSEN268-S25/tree/L05.00) Layout Concepts
+- [Lecture 5 - 01](https://github.com/mehmetartun/CSEN268-S25/tree/L05.00) Layout Concepts
