57913f41ec79aae26901055a3ddbdc8eed6006ce
Mehmet Artun <mehmet@artun.com>
2025-04-22 18:52:14 -0700

    L0702

--- lib/pages/login/views/initial_view.dart
+++ lib/pages/login/views/initial_view.dart
@@ -3,25 +3,87 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../cubit/login_cubit.dart';
 
-class InitialView extends StatelessWidget {
+class InitialView extends StatefulWidget {
   const InitialView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final String text = BlocProvider.of<LoginCubit>(context).className;
+  State<InitialView> createState() => _InitialViewState();
+}
 
+class _InitialViewState extends State<InitialView> {
+  final _formKey = GlobalKey<FormState>();
+  late LoginCubit cubit;
+  String? email;
+  String? password;
+  @override
+  void initState() {
+    // TODO: implement initState
+    cubit = BlocProvider.of<LoginCubit>(context);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Hello Login")),
-      body: Center(
+      appBar: AppBar(title: const Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            FilledButton(
-              onPressed: () {
-                BlocProvider.of<LoginCubit>(context).printSomething();
-              },
-              child: Text("Press here"),
+            Text(
+              "Authentication Status",
+              style: Theme.of(context).textTheme.labelMedium,
             ),
-            Text(text),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: "Email"),
+                    validator: (value) {
+                      // Also you can use value.trim() to avoid full of spaces...
+                      if (value == null ||
+                          value.isEmpty ||
+                          value.trim() == "") {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      email = value;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: "Password"),
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          value.trim() == "") {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      password = value;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: double.maxFinite,
+                    child: FilledButton(
+                      onPressed: () {
+                        if (_formKey.currentState?.validate() ?? false) {
+                          _formKey.currentState?.save();
+                          cubit.loginUser(email: email!, password: password!);
+                        }
+                      },
+                      child: const Text('Login'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
