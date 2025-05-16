071f8fbe984fef8fd8a01db6cb351a0da0e56cdf
Mehmet Artun <mehmet@artun.com>
2025-05-15 22:02:30 -0700

    stream

--- lib/pages/db/cubit/db_cubit.dart
+++ lib/pages/db/cubit/db_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
@@ -11,10 +13,37 @@ class DbCubit extends Cubit<DbState> {
 
   FirebaseFirestore db = FirebaseFirestore.instance;
 
+  late StreamSubscription<QuerySnapshot> vehiclesSubscription;
+  late StreamSubscription<QuerySnapshot> collectionGroupDemoSubscription;
+
   List<Car> cars = [];
 
   void init() async {
-    await Future.delayed(Duration(seconds: 2), () {});
+    collectionGroupDemoSubscription = db
+        .collectionGroup('cars')
+        .where('make', isEqualTo: 'bmw')
+        .where('model', isEqualTo: '330')
+        .snapshots()
+        .listen((qs) {
+          qs.docs.forEach((doc) {
+            print(doc.data());
+          });
+        });
+
+    vehiclesSubscription = db
+        .collection('vehicles')
+        .where('make', isEqualTo: "BMW")
+        .where('model', isEqualTo: "330i")
+        .snapshots()
+        .listen((qs) {
+          cars = [];
+          for (var i = 0; i < qs.docs.length; i++) {
+            // print(qs.docs[i].data());
+            cars.add(Car.fromFirestoreMap(qs.docs[i].data()));
+          }
+          emit(DbLoaded());
+        });
+    // await Future.delayed(Duration(seconds: 2), () {});
     emit(DbLoaded());
   }
 
@@ -22,5 +51,13 @@ class DbCubit extends Cubit<DbState> {
     emit(DbFetching());
     await db.collection('vehicles').add(car.toFirestoreMap());
     emit(DbLoaded());
+  }
+
+  @override
+  Future<void> close() {
+    // TODO: implement close
+    vehiclesSubscription.cancel();
+    collectionGroupDemoSubscription.cancel();
+    return super.close();
   }
 }
