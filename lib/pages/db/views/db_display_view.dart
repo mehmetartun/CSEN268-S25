b4ae7823893b4be792dfa1711d5fc69072791f0c
Mehmet Artun <mehmet@artun.com>
2025-05-15 19:24:00 -0700

    toc

--- lib/pages/db/views/db_display_view.dart
+++ lib/pages/db/views/db_display_view.dart
@@ -1,15 +1,32 @@
+import 'package:csen268_s25/pages/db/forms/vehicle_input_form.dart';
 import 'package:flutter/material.dart';
 
 import '../../../model/car.dart';
 
 class DbDisplayView extends StatelessWidget {
   final List<Car> cars;
-  const DbDisplayView({super.key, required this.cars});
+  final Function(Car) addCar;
+  const DbDisplayView({super.key, required this.cars, required this.addCar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Db View")),
+      appBar: AppBar(
+        title: Text("Db View"),
+        actions: [
+          TextButton(
+            child: Text("Add"),
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return Dialog(child: VehicleInputForm(onSaved: addCar));
+                },
+              );
+            },
+          ),
+        ],
+      ),
       body: ListView(
         children: [
           ...cars.map((car) {
